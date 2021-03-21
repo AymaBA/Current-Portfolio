531d170e07ffecf4d3c53f4ea77a75139225e271
Ayman <aymaprod12@gmail.com>
2021-03-22 00:29:26 +0100

    ajout du lien linkedin

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Contact() {
-	return (
-		<div className="flex flex-col items-center justify-center font-dosis pb-5" style={{ background: 'rgb(9, 28, 41)' }} id="contact">
-			<div className="text-white p-4">
-				<h1 className="text-4xl font-bold mb-3">About me :</h1>
-				<p className="text-2xl">My name is Ayman, I am 17 years old. I am a high school student, passionate about computers siences since always. I have been doing web programming for 1 years now.</p>
-			</div>
-			<div className="flex">
-				<a href="https://github.com/AymaBA" target="blank" className="w-12 mx-3 select-none">
-					<LazyLoadImage
-						src={process.env.PUBLIC_URL + '/assets/github.png'}
-					/>
-				</a>
-				<a href="mailto:aymanbadev@gmail.com?subject=Portfolio Message"  target="blank" className="w-12 select-none">
-					<LazyLoadImage
-						src={process.env.PUBLIC_URL + '/assets/gmail.png'}
-					/>
-				</a>
-			</div>
-		</div>
-	)
+  return (
+    <div
+      className="flex flex-col items-center justify-center font-dosis pb-5"
+      style={{ background: "rgb(9, 28, 41)" }}
+      id="contact"
+    >
+      <div className="text-white p-4">
+        <h1 className="text-4xl font-bold mb-3">About me :</h1>
+        <p className="text-2xl">
+          My name is Ayman, I am 17 years old. I am a high school student,
+          passionate about computers siences since always. I have been doing web
+          programming for 1 years now.
+        </p>
+      </div>
+      <div className="flex">
+        <a
+          href="https://github.com/AymaBA"
+          target="blank"
+          className="w-12 mx-3 select-none"
+        >
+          <LazyLoadImage src={process.env.PUBLIC_URL + "/assets/github.png"} />
+        </a>
+        <a
+          href="mailto:aymanbadev@gmail.com?subject=Portfolio Message"
+          target="blank"
+          className="w-12 select-none"
+        >
+          <LazyLoadImage src={process.env.PUBLIC_URL + "/assets/gmail.png"} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/aymaba/"
+          target="blank"
+          className="w-12 select-none"
+        >
+          <LazyLoadImage src={process.env.PUBLIC_URL + "/assets/link.png"} />
+        </a>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,55 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-		animation:{
-			'spin-slow': 'spin 3s linear infinite',
-		},
-		fontFamily:{
-			'dosis':['Dosis']
-		},
-		animation:{
-			float: 'float 3s infinite',
-			refloat: 'float 3s infinite 1s',
-			spawn:'spawn 1s'
-		},
-		keyframes: {
-			float: {
-				'0%': { transform: 'translateY(0)',
-					animationTimingFunction:'ease-in-out'
-				},
-				'50%': { transform: 'translateY(20px)',
-					animationTimingFunction:'ease-in-out'
-				},
-				'100%': { transform: 'translateY(0)',
-					animationTimingFunction:'ease-in-out'
-				},
-			},
-			spawn: {
-				'0%': { transform: 'translateX(-40px)',
-					opacity:0,
-					animationTimingFunction:'ease-in-out'
-				},
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+      fontFamily: {
+        dosis: ["Dosis"],
+      },
+      animation: {
+        float: "float 3s infinite",
+        refloat: "float 3s infinite 1s",
+        spawn: "spawn 1s",
+      },
+      keyframes: {
+        float: {
+          "0%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-in-out",
+          },
+          "50%": {
+            transform: "translateY(20px)",
+            animationTimingFunction: "ease-in-out",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "ease-in-out",
+          },
+        },
+        spawn: {
+          "0%": {
+            transform: "translateX(-40px)",
+            opacity: 0,
+            animationTimingFunction: "ease-in-out",
+          },
 
-				'100%': { transform: 'translateX(0)',
-					opacity:1,
-					animationTimingFunction:'ease-in-out'
-				}
-			}
-		},
-		transitionDuration: {
-			'2000': '2000ms',
-		}
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+            animationTimingFunction: "ease-in-out",
+          },
+        },
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-}
+};
