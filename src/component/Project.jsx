f46a52f801983d65f9ea840eea7847f861ad4f4c
Ayman <aymaprod12@gmail.com>
2021-05-30 19:12:11 +0200

    ajout d'un projet

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 function Project() {
-	return (
-		<div className="min-h-screen flex flex-col items-center justify-center font-dosis" id="project">
-			<div className="flex flex-col">
-				<h1 className="text-5xl font-bold text-center mt-20 md:mt-0 " id="title">My Project</h1>
-				<div className="flex justify-around items-center flex-row flex-wrap p-5">
-					<Card 
-						title="Post-it" 
-						description="Just a simple website that anyone can post things on." 
-						githubUrl="https://github.com/AymaBA/post-it"
-						imageUrl={process.env.PUBLIC_URL+"assets/post-it.png"}
-					/>
-					<Card 
-						title="Cloudbudget Integration" 
-						projectUrl="https://cloudbudget-ayman.netlify.app/"
-						description="Integration of a figma web model with React and Snowpack of HTML and CSS." 
-						githubUrl="https://github.com/AymaBA/Cloudbudget-Integration"
-						imageUrl={process.env.PUBLIC_URL+"assets/cloudbudget.png"}
-					/>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div
+      className="min-h-screen flex flex-col items-center justify-center font-dosis"
+      id="project"
+    >
+      <div className="flex flex-col">
+        <h1
+          className="text-5xl font-bold text-center mt-20 md:mt-0 "
+          id="title"
+        >
+          My Project
+        </h1>
+        <div className="flex justify-around items-center flex-row flex-wrap p-5">
+          <Card
+            title="Suplit"
+            description="Application allowing you to purchase and sell non-use course supplies."
+            githubUrl="https://github.com/AymaBA"
+            imageUrl={process.env.PUBLIC_URL + "assets/suplit.wepb"}
+          />
+          <Card
+            title="Post-it"
+            description="Just a simple website that anyone can post things on."
+            githubUrl="https://github.com/AymaBA/post-it"
+            imageUrl={process.env.PUBLIC_URL + "assets/post-it.png"}
+          />
+          <Card
+            title="Cloudbudget Integration"
+            projectUrl="https://cloudbudget-ayman.netlify.app/"
+            description="Integration of a figma web model with React and Snowpack of HTML and CSS."
+            githubUrl="https://github.com/AymaBA/Cloudbudget-Integration"
+            imageUrl={process.env.PUBLIC_URL + "assets/cloudbudget.png"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
